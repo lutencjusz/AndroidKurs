30e64db021b8cddd22d7ad6f65bce4aa59d871ef
Lutencjusz <lutencjusz@gmail.com>
2021-10-30 08:36:18 +0200

    tworzenie MainActivity i UserManager

--- app/src/main/java/com/example/kursapplication/LoginActivity.java
+++ app/src/main/java/com/example/kursapplication/LoginActivity.java
@@ -4,28 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.example.kursapplication.api.LoginResponse;
-import com.example.kursapplication.api.PodcastApi;
-import java.io.IOException;
-import java.lang.annotation.Annotation;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.github.cdimascio.dotenv.Dotenv;
-import okhttp3.OkHttpClient;
-import okhttp3.ResponseBody;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Converter;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -36,11 +21,27 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.edPassword)
     EditText edPassword;
 
+    private UserManager userManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        userManager = new UserManager();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        userManager.onAttach(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        userManager.onStop();
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -68,58 +69,18 @@ public class LoginActivity extends AppCompatActivity {
             hasErr = true;
         }
         if (!hasErr) {
-            login(email, password);
+            userManager.login(email, password);
         }
 
     }
 
-    private void login(String email, String password) {
 
-        Dotenv dotenv = (Dotenv) Dotenv.configure().directory("./assets").filename("env").load();
-
-        String id = dotenv.get("ID");
-        String key = dotenv.get("REST-API-KEY");
-
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-
-        OkHttpClient client = new OkHttpClient.Builder().addNetworkInterceptor(loggingInterceptor).build();
-
-        Retrofit.Builder builder = new Retrofit.Builder();
-        builder.baseUrl("https://parseapi.back4app.com/");
-        builder.addConverterFactory(GsonConverterFactory.create());
-        builder.client(client);
-        Retrofit retrofit = builder.build();
-        PodcastApi podcastApi = retrofit.create(PodcastApi.class);
-        Call<LoginResponse> call = podcastApi.getLogin(email, password, id, key);
-        call.enqueue(new Callback<LoginResponse>() {
-            @Override
-            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                if (response.isSuccessful()) {
-                    LoginResponse body = response.body();
-                    Log.d(LoginActivity.class.getSimpleName(), "Odpowiedź: " + body);
-                    Toast.makeText(LoginActivity.this, "Udało Ci się zalogować!!!", Toast.LENGTH_SHORT).show();
-                } else {
-                    try {
-                        ResponseBody responseBody = response.errorBody();
-                        Converter<ResponseBody, ErrorResponse> converter = retrofit.responseBodyConverter(ErrorResponse.class, new Annotation[]{});
-                        ErrorResponse errorResponse = converter.convert(responseBody);
-                        Toast.makeText(LoginActivity.this, "Błąd: " + errorResponse.error, Toast.LENGTH_SHORT).show();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<LoginResponse> call, Throwable t) {
-
-            }
-        });
-
+    public void loginSuccess() {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
     }
 
+    public void showError(String error) {
+        Toast.makeText(LoginActivity.this, error, Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/com/example/kursapplication/UserManager.java
@@ -0,0 +1,92 @@
+package com.example.kursapplication;
+
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import com.example.kursapplication.api.LoginResponse;
+import com.example.kursapplication.api.PodcastApi;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import io.github.cdimascio.dotenv.Dotenv;
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Converter;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class UserManager {
+
+    private LoginActivity loginActivity;
+
+    public void onAttach(LoginActivity loginActivity) {
+        this.loginActivity = loginActivity;
+    }
+
+    public void onStop() {
+        this.loginActivity = null;
+    }
+
+
+    public void login(String email, String password) {
+
+        Dotenv dotenv = Dotenv.configure().directory("./assets").filename("env").load();
+
+        String id = dotenv.get("ID");
+        String key = dotenv.get("REST-API-KEY");
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder().addNetworkInterceptor(loggingInterceptor).build();
+
+        Retrofit.Builder builder = new Retrofit.Builder();
+        builder.baseUrl("https://parseapi.back4app.com/");
+        builder.addConverterFactory(GsonConverterFactory.create());
+        builder.client(client);
+        Retrofit retrofit = builder.build();
+        PodcastApi podcastApi = retrofit.create(PodcastApi.class);
+        Call<LoginResponse> call = podcastApi.getLogin(email, password, id, key);
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<LoginResponse> call, @NonNull Response<LoginResponse> response) {
+                if (response.isSuccessful()) {
+                    LoginResponse body = response.body();
+                    Log.d(LoginActivity.class.getSimpleName(), "Odpowiedź: " + body);
+                    if (loginActivity != null) {
+                        loginActivity.loginSuccess();
+                    }
+                } else {
+                    try {
+                        ResponseBody responseBody = response.errorBody();
+                        Converter<ResponseBody, ErrorResponse> converter = retrofit.responseBodyConverter(ErrorResponse.class, new Annotation[]{});
+                        assert responseBody != null;
+                        ErrorResponse errorResponse = converter.convert(responseBody);
+                        assert errorResponse != null;
+                        if (loginActivity != null) {
+                            loginActivity.showError("Błąd: " + errorResponse.error);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<LoginResponse> call, @NonNull Throwable t) {
+                if (loginActivity != null) {
+                    loginActivity.showError("Błąd: " + t.getLocalizedMessage());
+                }
+
+            }
+        });
+
+    }
+}
