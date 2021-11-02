87aae992d9e794bbe3ab6244e3b10e34ddfbec32
Lutencjusz <lutencjusz@gmail.com>
2021-11-02 20:58:40 +0100

    utworzenie fragmentów

---
+++ app/src/main/java/com/example/kursapplication/screens/discover/DiscoverFragment.java
@@ -0,0 +1,18 @@
+package com.example.kursapplication.screens.discover;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.kursapplication.R;
+
+public class DiscoverFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.discover_fragment, container, false);
+    }
+}
