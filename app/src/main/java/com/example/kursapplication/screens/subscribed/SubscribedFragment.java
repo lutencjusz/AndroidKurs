ec7f75dd36de182056a3219ecc9ba9fbbd69d18c
Lutencjusz <lutencjusz@gmail.com>
2021-11-04 21:53:06 +0100

    dodawanie menu bocznych SubscribedFragment i

--- app/src/main/java/com/example/kursapplication/screens/subscribed/SubscribedFragment.java
+++ app/src/main/java/com/example/kursapplication/screens/subscribed/SubscribedFragment.java
@@ -3,6 +3,9 @@ package com.example.kursapplication.screens.subscribed;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
@@ -11,9 +14,32 @@ import androidx.fragment.app.Fragment;
 import com.example.kursapplication.R;
 
 public class SubscribedFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_subscribed, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.subscribe, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+                return true;
+            case R.id.action_sort:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
